3564b2e8c76af3273c58e1e953a2bad9e8f2bf16
LearningGp <arvinfield1998@outlook.com>
2020-09-23 13:03:19 +0800

    9.23

--- DocumenOp/DocumentOp.py
+++ DocumenOp/DocumentOp.py
@@ -8,26 +8,58 @@ import os
 import docx
 from win32com import client
 import re
+import xlwt
 
 if __name__ == '__main__':
     path = 'E:/Arvin/Document/WebCrawler/DocumenOp/documents'
     files = os.listdir(path)
     word = client.Dispatch("Word.Application")
     print(files)
+    names = []
+    locations = []
+    times = []
+    dates = []
     for file in files:
-        print(file)
-        file_path = 'E:/Arvin/Document/WebCrawler/DocumenOp/documents/' + file
-        print(file_path)
-        doc = word.Documents.Open(file_path)
-        doc.SaveAs(file_path+'x', 16)
-        doc.Close()
-        file_doc = docx.Document(file_path+'x')
-        text = file_doc.tables[1].rows[1].cells[0].text
-        time = re.search('\d{1,}[\u4e00-\u9fa5]{4,}', text)
-        location = re.search('[\u4e00-\u9fa5]{4,}.[\u4e00-\u9fa5]{1,}',text)
-        print(location.group(0))
-        print(time.group(0))
-        date = re.search('[\u4e00-\u9fa5]{5,}.{1,}', file_doc.paragraphs[0].text)
-        print(date.group(0))
+        try:
+            print(file)
+            Name = re.search('[_][\u4e00-\u9fa5]{1,3}[_]', file)
+            name = Name.group(0)
+            print(name[1:4])
+            names.append(name[1:4])
+            file_path = 'E:/Arvin/Document/WebCrawler/DocumenOp/documents/' + file
+            print(file_path)
+            doc = word.Documents.Open(file_path)
+            file_path2 = 'E:/Arvin/Document/WebCrawler/DocumenOp/documents2/' + file
+            doc.SaveAs(file_path2+'x', 16)
+            doc.Close()
+            file_doc = docx.Document(file_path2+'x')
+            text = file_doc.tables[1].rows[1].cells[0].text
+            time = re.search('\d{1,}[\u4e00-\u9fa5]{4,}', text)
+            try:
+                times.append(time.group(0))
+                print(time.group(0))
+            except:
+                times.append(0)
+            location = re.search('[\u4e00-\u9fa5]{4,}.[\u4e00-\u9fa5]{1,}', text)
+            try:
+                locations.append(location.group(0))
+                print(location.group(0))
+            except:
+                locations.append(0)
+            date = re.search('[\u4e00-\u9fa5]{5,}.{1,}', file_doc.paragraphs[0].text)
+            try:
+                dates.append(date.group(0))
+                print(date.group(0))
+            except:
+                dates.append(0)
+        except:
+            print(file, 'error')
     word.Quit()
-
+    wb = xlwt.Workbook()
+    sh1 = wb.add_sheet('sheet1')
+    for num in range(0, len(names)):
+        sh1.write(num, 0, names[num])
+        sh1.write(num, 1, locations[num])
+        sh1.write(num, 2, times[num])
+        sh1.write(num, 3, dates[num])
+    wb.save('test_w.xls')
--- DocumenOp/DocumentOp2.py
+++ DocumenOp/DocumentOp2.py
@@ -8,30 +8,16 @@ import os
 import docx
 from win32com import client
 import re
+import camelot
 
 if __name__ == '__main__':
     path = 'E:/Arvin/Document/WebCrawler/DocumenOp/documents'
     files = os.listdir(path)
-    word = client.Dispatch("Word.Application")
     print(files)
     for file in files:
         print(file)
         file_path = 'E:/Arvin/Document/WebCrawler/DocumenOp/documents/' + file
-        print(file_path)
-        doc = word.Documents.Open(file_path)
-        doc.SaveAs(file_path+'x', 16)
-        doc.Close()
-        file_doc = docx.Document(file_path+'x')
-        text = file_doc.tables[0].rows[0].cells[0].text
-        print(text)
-        for table in file_doc.tables:
-            for row in table.rows:
-                for cell in row.cells:
-                    print(cell.text)
-        # time = re.search('\d{1,}[\u4e00-\u9fa5]{4,}', text)
-        # location = re.search('[\u4e00-\u9fa5]{4,}.[\u4e00-\u9fa5]{1,}',text)
-        # print(location.group(0))
-        # print(time.group(0))
-        # date = re.search('[\u4e00-\u9fa5]{5,}.{1,}', file_doc.paragraphs[0].text)
-        # print(date.group(0))
-    word.Quit()
+        tables = camelot.read_pdf(file_path, pages='1', flavor='stream')
+        print(tables)
+        print(tables[0])
+        print(tables[0].data)

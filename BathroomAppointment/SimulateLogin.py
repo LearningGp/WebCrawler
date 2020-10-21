fec61a6c0d53413a08b47d0dc82800626697d1f4
LearningGp <arvinfield1998@outlook.com>
2020-10-21 20:27:01 +0800

    10.21

--- BathroomAppointment/SimulateLogin.py
+++ BathroomAppointment/SimulateLogin.py
@@ -12,19 +12,17 @@ from selenium.webdriver.chrome.options import Options
 import requests as rq
 if __name__ == '__main__':
     #  参数
-    myUsername = 'MF20320057'
-    myPassword = '3016XmHjh4127'
-    myName = '何家欢'
-    # myUsername = 'MF20320053'
-    # myPassword = 'gzhanuo@86'
-    # myName = '郭展'
-    beginTime = '2020-09-30 20:40'
-    endTime = '2020-09-30 21:00'
+    myUsername = ''
+    myPassword = ''
+    myName = ''
+    beginTime = '2020-10-15 21:40'
+    endTime = '2020-10-15 22:00'
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gup')
     bro = webdriver.Chrome(executable_path='./selenium/chromedriver.exe', chrome_options=chrome_options)
-    bro.get('http://authserver.nju.edu.cn/authserver/login?service=http%3A%2F%2Fehall.nju.edu.cn%2Flogin%3Fservice%3Dhttp%3A%2F%2Fehall.nju.edu.cn%2Fywtb-portal%2Fofficial%2Findex.html%23%2F')
+    bro.get('')
+    #  教务系统登录url
     userName = bro.find_element_by_id('username')
     passWord = bro.find_element_by_id('password')
     sleep(1)
@@ -42,7 +40,8 @@ if __name__ == '__main__':
     btn = bro.find_element_by_xpath('//*[@id="casLoginForm"]/p[4]/button')
     btn.click()
     sleep(2)
-    bro.get('http://ehallapp.nju.edu.cn/qljfwapp/sys/lwAppointmentBathroom/index.do?t_s=1599708219264&amp_sec_version_=1&gid_=Qm9nNDVML1laR1RkK3FtRGp5cGk5YmwvUHZkWTJ2VUpTUXFUYUpzMnhYMS8yS1g3azQ1dlh0UDdBRGwxOEJWRk5jYkVyL29kVjJUQzU3b0JRSU56d2c9PQ&EMAP_LANG=zh&THEME=magenta#/myAppointment')
+    bro.get('')
+    #  浴池预约url
     c = bro.get_cookies()
     bro.quit()
     cookies = {}
@@ -51,18 +50,19 @@ if __name__ == '__main__':
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36',
     }
+    #  补全对应信息
     formData_o = {"USER_ID": myUsername,
                   "USER_NAME": myName,
-                  "DEPT_CODE": "4270",
-                  "DEPT_NAME": "软件学院",
+                  "DEPT_CODE": "",
+                  "DEPT_NAME": "",
                   "PHONE_NUMBER": None,
                   "PALCE_ID": "f881e8c2aa6f4190bc3efa13408143af",
                   "BEGINNING_DATE": beginTime,
                   "ENDING_DATE": endTime,
                   "SCHOOL_DISTRICT_CODE": "02",
-                  "SCHOOL_DISTRICT": "鼓楼校区",
-                  "LOCATION": "鼓楼男浴室",
-                  "PLACE_NAME": "鼓楼男浴室",
+                  "SCHOOL_DISTRICT": "",
+                  "LOCATION": "",
+                  "PLACE_NAME": "",
                   "IS_CANCELLED": 0}
     formData = json.dumps(formData_o)
     param = {
